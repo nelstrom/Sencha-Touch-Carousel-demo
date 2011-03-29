e5f0fac7e9d861252380ac181851ae0022e04551
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-29 18:24:23 +0200

    Create horizontal and vertical carousel classes.

---
+++ public/app/views/horizontal_carousel.js
@@ -0,0 +1,5 @@
+App.views.HorizontalCarousel = Ext.extend(Ext.Carousel, {
+    initComponent: function() {
+        App.views.Viewport.superclass.initComponent.apply(this, arguments);
+    }
+});
---
+++ public/app/views/vertical_carousel.js
@@ -0,0 +1,5 @@
+App.views.VerticalCarousel = Ext.extend(Ext.Carousel, {
+    initComponent: function() {
+        App.views.Viewport.superclass.initComponent.apply(this, arguments);
+    }
+});
