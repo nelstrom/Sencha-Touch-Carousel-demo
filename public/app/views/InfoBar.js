72ad26ab1b8f8aacb738353a423dcd3f41b0f34e
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-22 19:12:20 +0300

    Switch from tabs to spaces.

--- public/app/views/InfoBar.js
+++ public/app/views/InfoBar.js
@@ -1,45 +1,45 @@
 App.views.InfoBar = Ext.extend(Ext.Panel, {
     initComponent: function(){
         var infoBarPanel = this,
-			activeCardIndex = 0;
+            activeCardIndex = 0;
 
         Ext.apply(this, {
 
             layout: 'card',
-			cardSwitchAnimation: 'flip',
+            cardSwitchAnimation: 'flip',
 
-			dockedItems: [
-				{
-					xtype: 'toolbar',
-					title: this.title,
-					items: [
-						{ xtype: 'spacer' },
-						{
-							text: 'info',
-							handler: function() {
-								if (activeCardIndex == 0) {
-									activeCardIndex = 1;
-								} else {
-									activeCardIndex = 0;
-								}
-								infoBarPanel.setActiveItem(activeCardIndex);
-							}
-						}
-					]
-				}
-			],
+            dockedItems: [
+                {
+                    xtype: 'toolbar',
+                    title: this.title,
+                    items: [
+                        { xtype: 'spacer' },
+                        {
+                            text: 'info',
+                            handler: function() {
+                                if (activeCardIndex == 0) {
+                                    activeCardIndex = 1;
+                                } else {
+                                    activeCardIndex = 0;
+                                }
+                                infoBarPanel.setActiveItem(activeCardIndex);
+                            }
+                        }
+                    ]
+                }
+            ],
             items: [
                 {
-					style: { backgroundColor: '#aaa' },
-					tpl: [
-						"<h2>{title}</h2>",
-						"<p>by {artist}</p>",
-						"<dl>",
-						"  <dt>medium:</dt><dd>{medium}</dd>",
-						"  <dt>dimensions:</dt><dd>{dimensions}</dd>",
-						"</dl>"
-					],
-					show: function() { this.update(infoBarPanel); }
+                    style: { backgroundColor: '#aaa' },
+                    tpl: [
+                        "<h2>{title}</h2>",
+                        "<p>by {artist}</p>",
+                        "<dl>",
+                        "  <dt>medium:</dt><dd>{medium}</dd>",
+                        "  <dt>dimensions:</dt><dd>{dimensions}</dd>",
+                        "</dl>"
+                    ],
+                    show: function() { this.update(infoBarPanel); }
                 },
                 {
                     cls: 'canvas ' + this.slug,
