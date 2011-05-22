333f0e14070aeb3e0c3094c289877980e9248f08
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-22 18:32:41 +0300

    Switch from instantiated objects to lightweight config options.

--- public/app/views/InfoSwipe.js
+++ public/app/views/InfoSwipe.js
@@ -2,26 +2,7 @@ App.views.InfoSwipe = Ext.extend(Ext.Panel, {
     initComponent: function(){
         var infoSwipePanel = this;
 
-        var titleCard = new Ext.Panel({
-            tpl: [
-                "<h2>{title}</h2>",
-                "<p>by {artist}</p>"
-            ],
-            show: function() { this.update(infoSwipePanel); }
-        });
-
-        var metadataCard = new Ext.Panel({
-            tpl: [
-                "<dl>",
-                "  <dt>medium:</dt><dd>{medium}</dd>",
-                "  <dt>dimensions:</dt><dd>{dimensions}</dd>",
-                "</dl>"
-            ],
-            show: function() { this.update(infoSwipePanel); }
-        });
-
         Ext.apply(this, {
-
 
             scroll: false,
 
@@ -38,7 +19,21 @@ App.views.InfoSwipe = Ext.extend(Ext.Panel, {
                 {
                     xtype: 'carousel',
                     flex: 1,
-                    items: [titleCard, metadataCard]
+                    items: [
+                        {
+                            tpl: ["<h2>{title}</h2>", "<p>by {artist}</p>"],
+                            show: function() { this.update(infoSwipePanel); }
+                        },
+                        {
+                            tpl: [
+                                "<dl>",
+                                "  <dt>medium:</dt><dd>{medium}</dd>",
+                                "  <dt>dimensions:</dt><dd>{dimensions}</dd>",
+                                "</dl>"
+                            ],
+                            show: function() { this.update(infoSwipePanel); }
+                        }
+                    ]
                 }
             ]
         });
