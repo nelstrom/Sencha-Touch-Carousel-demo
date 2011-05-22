0b2647a43603ca7955d4e7aae72695853d1d921a
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-22 15:56:57 +0300

    Add 'by Dale Adcock' line to each panel.

--- public/app/views/viewport.js
+++ public/app/views/viewport.js
@@ -9,40 +9,40 @@ App.views.Viewport = Ext.extend(Ext.Carousel, {
                 {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
-                        { cls: 'canvas asteroid', flex: 6 },
-                        { html: '<h2>Untitled asteroid</h2>' }
+                        { cls: 'canvas asteroid', flex: 3 },
+                        { html: '<h2>Untitled asteroid</h2><p>by Dale Adcock</p>' }
                     ]
                 },
 
                 {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
-						{ cls: 'canvas template', flex: 6 },
-                        { html: '<h2>Template</h2>' }
+						{ cls: 'canvas template', flex: 3 },
+                        { html: '<h2>Template</h2><p>by Dale Adcock</p>' }
                     ]
                 },
 
                 {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
-						{ cls: 'canvas standing', flex: 6 },
-                        { html: '<h2>Standing</h2>' }
+						{ cls: 'canvas standing', flex: 3 },
+                        { html: '<h2>Standing</h2><p>by Dale Adcock</p>' }
                     ]
                 },
 
                 {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
-						{ cls: 'canvas sitting', flex: 6 },
-                        { html: '<h2>Sitting</h2>' }
+						{ cls: 'canvas sitting', flex: 3 },
+                        { html: '<h2>Sitting</h2><p>by Dale Adcock</p>' }
                     ]
                 },
 
                 {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
-						{ cls: 'canvas erect', flex: 6 },
-                        { html: '<h2>Erect</h2>' }
+						{ cls: 'canvas erect', flex: 3 },
+                        { html: '<h2>Erect</h2><p>by Dale Adcock</p>' }
                     ]
                 },
             ],
