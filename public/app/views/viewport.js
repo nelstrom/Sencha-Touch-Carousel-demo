7edeb3f88ce8477c92bb424dcc41a0a2d7a15392
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-22 15:51:46 +0300

    Add info panels to each of the paintings.

--- public/app/views/viewport.js
+++ public/app/views/viewport.js
@@ -10,13 +10,41 @@ App.views.Viewport = Ext.extend(Ext.Carousel, {
                     layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
                     items: [
                         { cls: 'canvas asteroid', flex: 6 },
-                        { html: 'info' }
+                        { html: '<h2>Untitled asteroid</h2>' }
                     ]
                 },
-                { cls: 'canvas template' },
-                { cls: 'canvas standing' },
-                { cls: 'canvas sitting' },
-                { cls: 'canvas erect' },
+
+                {
+                    layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
+                    items: [
+						{ cls: 'canvas template', flex: 6 },
+                        { html: '<h2>Template</h2>' }
+                    ]
+                },
+
+                {
+                    layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
+                    items: [
+						{ cls: 'canvas standing', flex: 6 },
+                        { html: '<h2>Standing</h2>' }
+                    ]
+                },
+
+                {
+                    layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
+                    items: [
+						{ cls: 'canvas sitting', flex: 6 },
+                        { html: '<h2>Sitting</h2>' }
+                    ]
+                },
+
+                {
+                    layout: { type: 'vbox', align: 'stretch' }, defaults: {flex: 1},
+                    items: [
+						{ cls: 'canvas erect', flex: 6 },
+                        { html: '<h2>Erect</h2>' }
+                    ]
+                },
             ],
         });
         App.views.Viewport.superclass.initComponent.apply(this, arguments);
