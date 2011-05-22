2ac3f4be62735e42358b22a4924490cd962235d7
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-22 15:29:03 +0300

    Update viewport to load image switcher.

--- public/app/views/viewport.js
+++ public/app/views/viewport.js
@@ -6,14 +6,11 @@ App.views.Viewport = Ext.extend(Ext.Carousel, {
             cls: 'painting',
             defaults: { styleHtml: true },
             items: [
-                { cls: 'item-1' },
-                { cls: 'item-2' },
-                { cls: 'item-3' },
-                { cls: 'item-4' },
-                { cls: 'item-5' },
-                { cls: 'item-6' },
-                { cls: 'item-7' },
-                { cls: 'item-8' },
+                { cls: 'asteroid' },
+                { cls: 'template' },
+                { cls: 'standing' },
+                { cls: 'sitting' },
+                { cls: 'erect' },
             ],
         });
         App.views.Viewport.superclass.initComponent.apply(this, arguments);
