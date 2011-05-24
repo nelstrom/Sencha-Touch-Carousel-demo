a24c8bdfd08ec5ba724516fdfd0d408cd5fc65be
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-24 15:03:22 +0300

    Fix indentation to use spaces.

--- public/app/views/InfoBar.js
+++ public/app/views/InfoBar.js
@@ -2,44 +2,44 @@ App.views.InfoBar = Ext.extend(Ext.Panel, {
     initComponent: function(){
         var infoBarPanel = this,
 
-		imageCard = {
-			id:  'image_' + this.slug,
-			cls: 'canvas ' + this.slug,
-		},
+        imageCard = {
+            id:  'image_' + this.slug,
+            cls: 'canvas ' + this.slug,
+        },
 
-		infoCard = {
-			id: 'info_' + this.slug,
-			html: '<h3>{placeholder}</h3>',
-			styleHtmlContent: true,
-			tpl: [
-				"<h2>{title}</h2>",
-				"<p>by {artist}</p>",
-				"<dl>",
-				"  <dt>medium:</dt><dd>{medium}</dd>",
-				"  <dt>dimensions:</dt><dd>{dimensions}</dd>",
-				"</dl>"
-			],
-		},
+        infoCard = {
+            id: 'info_' + this.slug,
+            html: '<h3>{placeholder}</h3>',
+            styleHtmlContent: true,
+            tpl: [
+                "<h2>{title}</h2>",
+                "<p>by {artist}</p>",
+                "<dl>",
+                "  <dt>medium:</dt><dd>{medium}</dd>",
+                "  <dt>dimensions:</dt><dd>{dimensions}</dd>",
+                "</dl>"
+            ],
+        },
 
-		toggleButton = {
-			text: 'info',
-			handler: function() {
-				if (this.getText() == 'image') {
-					infoBarPanel.setActiveItem('image_' + infoBarPanel.slug);
-					this.setText('info');
-				} else {
-					infoBarPanel.setActiveItem('info_' + infoBarPanel.slug);
-					this.setText('image');
-				}
-			}
-		};
+        toggleButton = {
+            text: 'info',
+            handler: function() {
+                if (this.getText() == 'image') {
+                    infoBarPanel.setActiveItem('image_' + infoBarPanel.slug);
+                    this.setText('info');
+                } else {
+                    infoBarPanel.setActiveItem('info_' + infoBarPanel.slug);
+                    this.setText('image');
+                }
+            }
+        };
 
         Ext.apply(this, {
 
             layout: 'card',
             cardSwitchAnimation: 'flip',
 
-			items: [imageCard, infoCard],
+            items: [imageCard, infoCard],
 
             dockedItems: [
                 {
@@ -49,12 +49,12 @@ App.views.InfoBar = Ext.extend(Ext.Panel, {
                 }
             ],
 
-			listeners: {
-				beforecardswitch: function() {
-					var infoPanel = this.getComponent('info_' + this.slug);
-					infoPanel.update(this);
-				}
-			}
+            listeners: {
+                beforecardswitch: function() {
+                    var infoPanel = this.getComponent('info_' + this.slug);
+                    infoPanel.update(this);
+                }
+            }
         });
 
         App.views.InfoBar.superclass.initComponent.call(this);
