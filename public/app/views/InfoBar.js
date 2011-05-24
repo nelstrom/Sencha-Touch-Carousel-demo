f1eb65dbe0e71f9988083fb9982eb7857721b5e5
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-24 15:02:36 +0300

    Refactor the InfoBar class.

--- public/app/views/InfoBar.js
+++ public/app/views/InfoBar.js
@@ -2,54 +2,52 @@ App.views.InfoBar = Ext.extend(Ext.Panel, {
     initComponent: function(){
         var infoBarPanel = this,
 
-            imageCard = {
-				id:  'image_' + this.slug,
-                cls: 'canvas ' + this.slug,
-            },
+		imageCard = {
+			id:  'image_' + this.slug,
+			cls: 'canvas ' + this.slug,
+		},
 
-            infoCard = {
-				id: 'info_' + this.slug,
-                html: '<h3>{placeholder}</h3>',
-                styleHtmlContent: true,
-                tpl: [
-                    "<h2>{title}</h2>",
-                    "<p>by {artist}</p>",
-                    "<dl>",
-                    "  <dt>medium:</dt><dd>{medium}</dd>",
-                    "  <dt>dimensions:</dt><dd>{dimensions}</dd>",
-                    "</dl>"
-                ],
+		infoCard = {
+			id: 'info_' + this.slug,
+			html: '<h3>{placeholder}</h3>',
+			styleHtmlContent: true,
+			tpl: [
+				"<h2>{title}</h2>",
+				"<p>by {artist}</p>",
+				"<dl>",
+				"  <dt>medium:</dt><dd>{medium}</dd>",
+				"  <dt>dimensions:</dt><dd>{dimensions}</dd>",
+				"</dl>"
+			],
+		},
 
-            };
+		toggleButton = {
+			text: 'info',
+			handler: function() {
+				if (this.getText() == 'image') {
+					infoBarPanel.setActiveItem('image_' + infoBarPanel.slug);
+					this.setText('info');
+				} else {
+					infoBarPanel.setActiveItem('info_' + infoBarPanel.slug);
+					this.setText('image');
+				}
+			}
+		};
 
         Ext.apply(this, {
 
             layout: 'card',
             cardSwitchAnimation: 'flip',
 
+			items: [imageCard, infoCard],
+
             dockedItems: [
                 {
                     xtype: 'toolbar',
                     title: this.title,
-                    items: [
-                        { xtype: 'spacer' },
-                        {
-                            text: 'info',
-                            handler: function() {
-								if (this.getText() == 'image') {
-									infoBarPanel.setActiveItem('image_' + infoBarPanel.slug);
-									this.setText('info');
-								} else {
-									infoBarPanel.setActiveItem('info_' + infoBarPanel.slug);
-									this.setText('image');
-								}
-                            }
-                        }
-                    ]
+                    items: [ { xtype: 'spacer' }, toggleButton ]
                 }
             ],
-
-			items: [imageCard, infoCard],
 
 			listeners: {
 				beforecardswitch: function() {
