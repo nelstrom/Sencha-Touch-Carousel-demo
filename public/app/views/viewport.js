275d12fb7681f7eee77f30f5caeb8fd7d7a5eace
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-25 11:16:24 +0300

    Update each carousel item to use vbox layout.

--- public/app/views/viewport.js
+++ public/app/views/viewport.js
@@ -3,13 +3,78 @@ App.views.Viewport = Ext.extend(Ext.Carousel, {
     
     initComponent: function() {
         Ext.apply(this, {
-            cls: 'painting',
+            defaults: {
+                layout: {
+                    type: 'vbox',
+                    align: 'stretch'
+                }
+            },
             items: [
-                { cls: 'asteroid' },
-                { cls: 'template' },
-                { cls: 'standing' },
-                { cls: 'sitting' },
-                { cls: 'erect' },
+                {
+                    items: [
+                        {
+                            flex: 3,
+                            cls: 'painting asteroid',
+                        },
+                        {
+                            flex: 1,
+                            styleHtmlContent: true,
+                            html: "<h3>Asteroid</h3><p>by Dale Adcock</p>"
+                        }
+                    ]
+                },
+                {
+                    items: [
+                        {
+                            flex: 3,
+                            cls: 'painting template',
+                        },
+                        {
+                            flex: 1,
+                            styleHtmlContent: true,
+                            html: "<h3>Template</h3><p>by Dale Adcock</p>"
+                        }
+                    ]
+                },
+                {
+                    items: [
+                        {
+                            flex: 3,
+                            cls: 'painting standing',
+                        },
+                        {
+                            flex: 1,
+                            styleHtmlContent: true,
+                            html: "<h3>Standing</h3><p>by Dale Adcock</p>"
+                        }
+                    ]
+                },
+                {
+                    items: [
+                        {
+                            flex: 3,
+                            cls: 'painting sitting',
+                        },
+                        {
+                            flex: 1,
+                            styleHtmlContent: true,
+                            html: "<h3>Sitting</h3><p>by Dale Adcock</p>"
+                        }
+                    ]
+                },
+                {
+                    items: [
+                        {
+                            flex: 3,
+                            cls: 'painting erect',
+                        },
+                        {
+                            flex: 1,
+                            styleHtmlContent: true,
+                            html: "<h3>Erect</h3><p>by Dale Adcock</p>"
+                        }
+                    ]
+                }
             ],
         });
         App.views.Viewport.superclass.initComponent.apply(this, arguments);
