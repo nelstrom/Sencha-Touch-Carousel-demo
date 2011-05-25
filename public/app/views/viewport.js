32ba37a7cb41a84ecf6c72a7624d9930c460cc01
Drew Neil <andrew.jr.neil@gmail.com>
2011-05-25 10:52:33 +0300

    Create a carousel with 5 cards for images.

--- public/app/views/viewport.js
+++ public/app/views/viewport.js
@@ -1,7 +1,17 @@
-App.views.Viewport = Ext.extend(Ext.Panel, {
+App.views.Viewport = Ext.extend(Ext.Carousel, {
     fullscreen: true,
     
     initComponent: function() {
+        Ext.apply(this, {
+            cls: 'painting',
+            items: [
+                { cls: 'asteroid' },
+                { cls: 'template' },
+                { cls: 'standing' },
+                { cls: 'sitting' },
+                { cls: 'erect' },
+            ],
+        });
         App.views.Viewport.superclass.initComponent.apply(this, arguments);
     }
 });
